b4820d36adb54cc04017cb36aa6d45f859a3dced
daxiazhanshenzhao <153145899+daxiazhanshenzhao@users.noreply.github.com>
2025-12-26 16:15:16 +0800

    姑姑嘎嘎

--- build/kubejs/server_scripts/main.js
+++ build/kubejs/server_scripts/main.js
@@ -1,3 +1,10 @@
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded server example script)')
 
+
+ServerEvents.tick((event) => {
+    let server = event.getServer();
+    server.getPlayers().forEach((player) => {
+        
+    });
+})
